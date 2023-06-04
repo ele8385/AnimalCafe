86e8b840928efdc233d919c57cb0ca57a135cc9e
ele8385 <ele8385@gmail.com>
2023-06-04 13:44:25 +0900

    .
    .

---
+++ Assets/Script/UIEffects.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIEffects : MonoBehaviour
+{
+    [SerializeField] private float scaleDuration = 0.2f;
+    [SerializeField] private float scaleMultiplier = 1.2f;
+
+    private Vector3 originalScale;
+    private Tweener tweener;
+
+    public void ScaleEffect(GameObject target)
+    {
+        if (tweener != null && tweener.IsActive())
+        {
+            //tweener.Kill();
+            target.transform.localScale = originalScale;
+        }
+        originalScale = transform.localScale;
+
+        // 버튼 클릭 시 이펙트 실행
+        tweener = target.transform.DOScale(originalScale * scaleMultiplier, scaleDuration)
+            .SetEase(Ease.OutBack)
+            .OnComplete(() => target.transform.DOScale(originalScale, scaleDuration)
+                .SetEase(Ease.OutBack));
+    }
+    
+}
